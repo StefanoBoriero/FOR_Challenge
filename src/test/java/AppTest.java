5d5aee63845762b60c02cbeab8ff7b1621816b49
Stefano <stefano.boriero@gmail.com>
2017-01-17 13:47:05 +0100

    Added tests for application

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by Stefano on 17/01/2017.
+ */
+public class AppTest {
+    @Test
+    public void test(){
+        //Some really hard test ;)
+        assertTrue(!false);
+    }
+}
